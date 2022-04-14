e19f9c989445c1113ae4e92cbb02c6409060aad5
Yma Het <yma.het@gmail.com>
2022-04-14 21:23:10 +0300

    Implemented validator efficiency gauge

--- deploy.sh
+++ deploy.sh
@@ -7,19 +7,10 @@ pushd /usr/src/mytonctrl; git pull origin master; popd
 python3 -c 'import sys; sys.path.append("/usr/src/mytonctrl"); import mytoninstaller; mytoninstaller.Init(); mytoninstaller.CreateLocalConfig(mytoninstaller.GetInitBlock(), localConfigPath="/usr/src/validator-monitoring/local.config.json")'
 
 wget -O /etc/datadog-agent/conf.d/lite_clien_last_block_age_seconds.yaml $REPO_PREFIX/lite_clien_last_block_age_seconds.yaml
-#cp lite_clien_last_block_age_seconds.yaml /etc/datadog-agent/conf.d/lite_clien_last_block_age_seconds.yaml
 wget -O /etc/datadog-agent/checks.d/lite_clien_last_block_age_seconds.py $REPO_PREFIX/lite_clien_last_block_age_seconds.py
-#cp lite_clien_last_block_age_seconds.py /etc/datadog-agent/checks.d/lite_clien_last_block_age_seconds.py
-#wget -O /etc/datadog-agent/conf.d/var_ton_work_db_files_packages_size.yaml $REPO_PREFIX/var_ton_work_db_files_packages_size.yaml
-#cp var_ton_work_db_files_packages_size.yaml /etc/datadog-agent/conf.d/var_ton_work_db_files_packages_size.yaml
-#wget -O /etc/datadog-agent/checks.d/var_ton_work_db_files_packages_size.py $REPO_PREFIX/var_ton_work_db_files_packages_size.py
-#cp var_ton_work_db_files_packages_size.py /etc/datadog-agent/checks.d/var_ton_work_db_files_packages_size.py
-#chmod o+x /var/ton-work/db/files/
-#chmod -R o+r /var/ton-work/db/files/packages/
-#chmod o+x /var/ton-work/db/files/packages/temp.archive.*.index
-#wget -O /etc/datadog-agent/conf.d/directory.d/conf.yaml https://raw.githubusercontent.com/tonwhales/validator-monitoring/main/directory_conf.yaml
+wget -O /etc/datadog-agent/conf.d/validator_efficiency.yaml $REPO_PREFIX/validator_efficiency.yaml
+wget -O /etc/datadog-agent/checks.d/validator_efficiency.py $REPO_PREFIX/validator_efficiency.py
 rm /etc/datadog-agent/conf.d/directory.d/conf.yaml
-#cp directory_conf.yaml /etc/datadog-agent/conf.d/directory.d/conf.yaml
 sed -i 's@# process_config@process_config:\n  enabled: "true"@g' /etc/datadog-agent/datadog.yaml
 sed -i 's@^process_config$@process_config:@g' /etc/datadog-agent/datadog.yaml
 sed -i 's@enabled: "true":$@enabled: "true"@g' /etc/datadog-agent/datadog.yaml
---
+++ validator_efficiency.py
@@ -0,0 +1,30 @@
+import sys
+sys.path.append("/usr/src/mytonctrl")
+import mytonctrl
+import mytoncore
+
+# the following try/except block will make the custom check compatible with any Agent version
+try:
+    # first, try to import the base class from new versions of the Agent...
+    from datadog_checks.base import AgentCheck
+except ImportError:
+    # ...if the above failed, the check is running in Agent version < 6.6.0
+    from checks import AgentCheck
+
+# content of the special variable __version__ will be shown in the Agent status page
+__version__ = "1.0.0"
+
+class ValidatorEfficiencyCheck(AgentCheck):
+    def send_gauge(self, value):
+         self.gauge('ton.validator.efficiency', value, tags=['SERVICE:validator-efficiency'] + self.instance.get('tags', []))
+
+
+    def check(self, instance):
+        mytoncore.local.buffer["localdbFileName"] = "/usr/local/bin/mytoncore/mytoncore.db"
+        toncore = mytonctrl.MyTonCore()
+        toncore.liteClient.configPath = "/usr/src/validator-monitoring/local.config.json"
+        mytoncore.local.db["liteServers"] = [0]
+        if len(list(filter(lambda v: v["adnlAddr"] == toncore.adnlAddr, toncore.GetConfig34()["validators"]))) != 0:
+            self.send_gauge(toncore.GetValidatorEfficiency(adnlAddr=toncore.adnlAddr))
+        else:
+            self.send_gauge(100.0)
