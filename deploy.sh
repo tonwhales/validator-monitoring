50c9d78a92e7aa82b2886b9f1e36d3a926f23f3a
Yma Het <yma.het@gmail.com>
2022-04-28 18:43:00 +0300

    Added role tag

--- deploy.sh
+++ deploy.sh
@@ -21,18 +21,21 @@ mkdir /usr/src/validator-monitoring/
 wget -O /usr/src/validator-monitoring/common.py $REPO_PREFIX/common.py
 python3 -c 'import sys; sys.path.append("/usr/src/mytonctrl"); import mytoninstaller; mytoninstaller.Init(); mytoninstaller.CreateLocalConfig(mytoninstaller.GetInitBlock(), localConfigPath="/usr/src/validator-monitoring/local.config.json")'
 ENVIRONMENT=$(python3 -c 'import sys; sys.path.append("/usr/src/validator-monitoring"); import common; print(common.EnvEnrichedConsumer().get_environment())')
+ROLE="undefined"
 
 wget -O /etc/datadog-agent/conf.d/lite_clien_last_block_age_seconds.yaml $REPO_PREFIX/lite_clien_last_block_age_seconds.yaml
 wget -O /etc/datadog-agent/checks.d/lite_clien_last_block_age_seconds.py $REPO_PREFIX/lite_clien_last_block_age_seconds.py
 
 if [ "$ARCHIVE_NODE" == "YES" ]; then
     wget -O /etc/datadog-agent/conf.d/nginx.d/conf.yaml $REPO_PREFIX/conf.d_nginx.d_conf.yaml
+    ROLE="archive"
 else
     wget -O /opt/datadog-agent/embedded/lib/python3.8/lib-dynload/readline.cpython-38-x86_64-linux-gnu.so $REPO_PREFIX/readline.cpython-38-x86_64-linux-gnu.so
     wget -O /etc/datadog-agent/conf.d/validator_efficiency.yaml $REPO_PREFIX/validator_efficiency.yaml
     wget -O /etc/datadog-agent/checks.d/validator_efficiency.py $REPO_PREFIX/validator_efficiency.py
     wget -O /etc/datadog-agent/conf.d/ton_validation_cycles.yaml $REPO_PREFIX/ton_validation_cycles.yaml
     wget -O /etc/datadog-agent/checks.d/ton_validation_cycles.py $REPO_PREFIX/ton_validation_cycles.py
+    ROLE="validator"
 fi
 rm /etc/datadog-agent/conf.d/directory.d/conf.yaml
 rm /etc/datadog-agent/conf.d/var_ton_work_db_files_packages_size.yaml
@@ -40,7 +43,7 @@ rm /etc/datadog-agent/checks.d/var_ton_work_db_files_packages_size.py
 sed -i 's@# process_config@process_config:\n  enabled: "true"@g' /etc/datadog-agent/datadog.yaml
 sed -i 's@^process_config$@process_config:@g' /etc/datadog-agent/datadog.yaml
 sed -i 's@enabled: "true":$@enabled: "true"@g' /etc/datadog-agent/datadog.yaml
-sed -i "s@^# tags:@tags:\n  - environment:$ENVIRONMENT@g" /etc/datadog-agent/datadog.yaml
+sed -i "s@^# tags:@tags:\n  - environment:$ENVIRONMENT\n  - role:$ROLE@g" /etc/datadog-agent/datadog.yaml
 
 
 wget -O /usr/src/validator-monitoring/ton_db_size.py $REPO_PREFIX/ton_db_size.py
