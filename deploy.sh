97e035cb5492d0ad2d2043c951750635b772fa3e
Yma Het <yma.het@gmail.com>
2022-05-16 23:14:27 +0300

    Added check for datadog-agent service before executing maon part of deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,15 @@ containsElement () {
   return 1
 }
 
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 declare -A ROLES
 ROLES[validator]="validator"
 ROLES[archive]="archive"
@@ -33,6 +42,12 @@ while [[ $# -gt 0 ]]; do
 done
 
 REPO_PREFIX=https://raw.githubusercontent.com/tonwhales/validator-monitoring/main
+
+
+if [ ! service_exists datadog-agent ]; then
+  echo "Datadog package needs to be installed before deploying."
+  exit 1
+fi
 
 # we need 1c78056a4249672a8e9eb0548c79e02d3ce19d5e
 pushd /usr/src/mytonctrl; git pull origin master; popd
